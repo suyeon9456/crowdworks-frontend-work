af9daa01f456b14b30182c5122fab5bee0bb2657
suyeon9456 <suyeon9456@naver.com>
2025-05-31 18:33:43 +0900

    feat: PdfUrlViewer 컴포넌트를 App에 추가하고 pdf.worker.min.js 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import './App.css';
+import PdfUrlViewer from './components/pdf/PdfUrlViewer';
+import * as pdfjs from 'pdfjs-dist';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <PdfUrlViewer />
+    </>
+  );
 }
 
 export default App;
