8d0531fe41d273593cf6302421f6b600b4576d08
suyeon9456 <suyeon9456@naver.com>
2025-06-01 20:04:29 +0900

    feat: PdfJsonProvider로 PdfUrlViewer 및 JsonTextBlockViewer를 감쌈

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import * as pdfjs from 'pdfjs-dist';
 import { useJsonData } from './hooks/useJsonData';
 import { useGroupedContent } from './hooks/useGroupedContent';
 import JsonTextBlockViewer from './components/json/JsonTextBlockViewer';
+import { PdfJsonProvider } from './contexts/PdfJsonContext';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function App() {
@@ -12,8 +13,10 @@ function App() {
 
   return (
     <div style={{ display: 'flex' }}>
-      <PdfUrlViewer />
-      <JsonTextBlockViewer jsonData={jsonData} groupedContent={groupedContent} />
+      <PdfJsonProvider>
+        <PdfUrlViewer />
+        <JsonTextBlockViewer jsonData={jsonData} groupedContent={groupedContent} />
+      </PdfJsonProvider>
     </div>
   );
 }
