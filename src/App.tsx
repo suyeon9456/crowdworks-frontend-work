8a979057e20a750e6e8f894dd70fca137559c617
suyeon9456 <suyeon9456@naver.com>
2025-06-01 16:18:40 +0900

    feat: App 컴포넌트에 JSON 데이터 렌더링 기능 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import './App.css';
 import PdfUrlViewer from './components/pdf/PdfUrlViewer';
 import * as pdfjs from 'pdfjs-dist';
-
+import { useJsonData } from './hooks/useJsonData';
+import { useGroupedContent } from './hooks/useGroupedContent';
+import JsonTextBlockViewer from './components/json/JsonTextBlockViewer';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function App() {
+  const { jsonData, elementRegistry } = useJsonData();
+  const groupedContent = useGroupedContent(jsonData, elementRegistry);
+
   return (
-    <>
+    <div style={{ display: 'flex' }}>
       <PdfUrlViewer />
-    </>
+      <JsonTextBlockViewer jsonData={jsonData} groupedContent={groupedContent} />
+    </div>
   );
 }
 
