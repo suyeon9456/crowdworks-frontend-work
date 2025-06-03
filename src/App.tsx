4d5f9aa414084de8ddf9b36216ae9f333b677f1b
suyeon9456 <suyeon9456@naver.com>
2025-06-03 22:43:08 +0900

    feat: PdfJsonInteractivePage 컴포넌트 추가 및 App 컴포넌트에서 기존 PDF 및 JSON 렌더링 로직 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,8 @@
 import './App.css';
-import PdfViewer from './components/features/pdf-viewer/PdfViewer';
-import * as pdfjs from 'pdfjs-dist';
-import useJsonData from './hooks/useJsonData';
-import useJsonDataParsing from './hooks/useJsonDataParsing';
-import { PdfJsonProvider } from './contexts/PdfJsonContext';
-import JsonRenderer from './components/features/json-viewer/JsonRenderer';
-import usePdfData from './hooks/usePdfData';
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+import PdfJsonInteractivePage from './pages/PdfJsonInteractivePage';
 
 function App() {
-  const { pdfData } = usePdfData({ pdfUrl: '/1.report.pdf' });
-  const { jsonData } = useJsonData({ jsonUrl: '/1.report.json' });
-  const parsedJsonData = useJsonDataParsing(jsonData);
-
-  return (
-    <div style={{ display: 'flex' }}>
-      <PdfJsonProvider>
-        <PdfViewer pdfData={pdfData} />
-        <JsonRenderer jsonData={jsonData} parsedJsonData={parsedJsonData} />
-      </PdfJsonProvider>
-    </div>
-  );
+  return <PdfJsonInteractivePage />;
 }
 
 export default App;
